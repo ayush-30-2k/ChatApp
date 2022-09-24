3d204d013665a2fa46b69448bca6467c287c27d4
Ayush Mishra <ayush@Ayushs-MacBook-Air.local>
2022-09-24 22:39:42 +0530

    changed now

--- src/firebase.js
+++ src/firebase.js
@@ -5,15 +5,15 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    projectId: process.env.REACT_APP_PROJECT_ID,
-    databaseURL: process.env.REACT_APP_DATABASE_URL,
-    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_APP_ID,
-    measurementId: process.env.REACT_APP_MEASUREMENT_ID
+    apiKey: "AIzaSyCEyg00W31lqYMWjsngqiGlXVXIWPlGyug",
+    authDomain: "chat-app-6b231.firebaseapp.com",
+    projectId: "chat-app-6b231",
+    storageBucket: "chat-app-6b231.appspot.com",
+    messagingSenderId: "1094366414667",
+    appId: "1:1094366414667:web:e764a548524cb1dcc6cace",
+    measurementId: "G-TSTJNT6YQF"
 };
+
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
