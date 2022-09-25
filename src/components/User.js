2c842d9ba4e3eb1df4e24dcb59010316cccd439c
Ayush Mishra <ayush@Ayushs-MacBook-Air.local>
2022-09-25 23:58:29 +0530

    finally completed

--- src/components/User.js
+++ src/components/User.js
@@ -21,7 +21,7 @@ export const User = ({ user, selectUser, user1, chat }) => {
     }
 
     const Style = {
-        fontSize: len(text) > 8 ? "0.66rem" : "default",
+        fontSize: len(text) > 8 ? "0.7rem" : "default",
     }
     return (
         <>
@@ -30,7 +30,7 @@ export const User = ({ user, selectUser, user1, chat }) => {
                     <div className="user_detail">
                         <img src={user.avatar || Img} alt="avatar" className='avatar' />
                         <div className='wrap_msg_notif'>
-                            <h4 style={Style}>{len(user.name) < 8 ? user.name : `${user.name.slice(0, 18)}....`} </h4>
+                            <h4 style={Style}>{len(user.name) < 8 ? user.name : `${user.name.slice(0, 6)}....`} </h4>
                             {data && (
                                 <>
                                     <p className={`truncate ${data.from === user1 ? 'meChat' : 'youChat'}`}>
